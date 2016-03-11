7abd360c3c3a81e4a5817385a8b6412c99a49af7
AjitSingh <jeetsingh.ajit@gmail.com>
2016-03-11 09:10:21 +0530

    [seesion2] adding floating button

--- app/src/main/java/ajitsingh/com/androiduisessions/session2/Session2.java
+++ app/src/main/java/ajitsingh/com/androiduisessions/session2/Session2.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import ajitsingh.com.androiduisessions.R;
+import ajitsingh.com.androiduisessions.demo.Demo;
 
 public class Session2 extends AppCompatActivity {
 
@@ -18,5 +19,9 @@ public class Session2 extends AppCompatActivity {
   public void pullToRefresh(View view) {
     final Intent intent = new Intent(this, PullToRefresh.class);
     startActivity(intent);
+  }
+
+  public void floatingButton(View view) {
+    Demo.start(this, R.layout.floating_button);
   }
 }
