e2505a2d53a795801bb49a22e5879552e187dcae
AjitSingh <jeetsingh.ajit@gmail.com>
2016-03-27 17:21:41 +0530

    [session 2] adding Sticky Header

--- app/src/main/java/ajitsingh/com/androiduisessions/session2/Session2.java
+++ app/src/main/java/ajitsingh/com/androiduisessions/session2/Session2.java
@@ -34,4 +34,9 @@ public class Session2 extends AppCompatActivity {
     Intent intent = new Intent(this, DraggableView.class);
     startActivity(intent);
   }
+
+  public void stickyHeader(View view) {
+    Intent intent = new Intent(this, StickyHeader.class);
+    startActivity(intent);
+  }
 }
